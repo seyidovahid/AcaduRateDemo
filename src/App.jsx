4e08bdc289363de0557408fdbeb9bb053a137287
seyidovahid <seyidovahid@gmail.com>
2023-10-06 00:22:25 +0400

    changes3

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">
-                Click on the Vite and React logos to learn more ffff
+                Click on the Vite and React logos to learn more ffffff
             </p>
         </>
     );
